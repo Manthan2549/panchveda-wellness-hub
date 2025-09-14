f3bd094d4386cf1fa65744aeb1f851c15f43038c
Manthan2549 <manthan90532@gmail.com>
2025-09-15 03:02:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
+himport { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -46,7 +46,7 @@ const App = () => (
           <Route path="/questionnaire" element={<HealthQuestionnaire />} />
 {/*         <Route path="/" element={<Features />} /> */}
 {/*         <Route path="/features" element={<Features />} /> */}
-{   <Route path="/health-questionnaire" element={<HealthQuestionnaire />} /> {/* ✅ new route */}
+{   <Route path="/healthquestionnaire" element={<HealthQuestionnaire />} /> {/* ✅ new route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
